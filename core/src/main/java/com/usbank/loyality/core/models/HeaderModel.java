093c0f1a1ed8696375f3e5981b4b9f3d38bc4df2
CORP\pannem <pavani.annem@epsilon.com>
2018-09-29 10:49:40 +0530

    changes for template structure

---
+++ core/src/main/java/com/usbank/loyality/core/models/HeaderModel.java
@@ -0,0 +1,114 @@
+package com.usbank.loyality.core.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import com.usbank.loyality.core.constants.GlobalConstants;
+import com.usbank.loyality.core.utils.CommonUtil;
+import com.usbank.loyality.core.utils.LinksUtil;
+import com.usbank.loyality.core.vo.LinkVO;
+
+/**
+ * The Class HeaderModel.
+ *
+ * @author ajena
+ */
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HeaderModel {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(HeaderModel.class);
+
+	private String logoImage;
+	private String logoAltText;
+	private String logoUrl;
+	private String logoNewWindow;
+
+	private List<LinkVO> listOfNavigations;
+
+	@SlingObject
+	private ResourceResolver resourceResolver;
+
+	@SlingObject
+	private Resource resource;
+
+	@PostConstruct
+	protected void init() {
+		log.info("HeaderModel :: init :: Start");
+		InheritanceValueMap iValueMap = new HierarchyNodeInheritanceValueMap(
+				resource);
+
+		getLogo(iValueMap);
+		getPrimaryNav(iValueMap);
+
+		log.info("HeaderModel :: init :: End");
+	}
+
+	private void getLogo(InheritanceValueMap iValueMap) {
+		if (null != iValueMap) {
+			logoImage = iValueMap.getInherited("logoImage", String.class);
+			logoAltText = iValueMap.getInherited("logoAltText", String.class);
+			logoUrl = LinksUtil.checkInternalURLByPath(
+					iValueMap.getInherited("logoUrl", String.class),
+					resourceResolver);
+			logoNewWindow = CommonUtil.isNewWindow(iValueMap.getInherited(
+					"logoNewWindow", GlobalConstants.FALSE));
+		}
+	}
+
+	private void getPrimaryNav(InheritanceValueMap iValueMap) {
+
+		// Primary Navigation links reading logic start
+
+		this.listOfNavigations = new ArrayList<>();
+		String[] navigationfields = iValueMap.getInherited("primarynavfields",
+				String[].class);
+		try {
+			if (navigationfields != null && navigationfields.length > 0) {
+				for (String navfield : navigationfields) {
+					LinkVO linkVO = (LinkVO) CommonUtil.getObjectFromJson(
+							navfield, new LinkVO());
+					linkVO.setLinkUrl(LinksUtil.checkInternalURLByPath(
+							linkVO.getLinkUrl(), resourceResolver));
+					linkVO.setLinkNewWindow(LinksUtil.isNewWindow(linkVO
+							.getLinkNewWindow()));
+					this.listOfNavigations.add(linkVO);
+				}
+			}
+		} catch (Exception e) {
+			log.error("HeaderModel :: getPrimaryNav :: JSONException :: {}", e);
+		}
+	}
+
+	public String getLogoImage() {
+		return logoImage;
+	}
+
+	public String getLogoAltText() {
+		return logoAltText;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public String getLogoNewWindow() {
+		return logoNewWindow;
+	}
+
+	public List<LinkVO> getListOfNavigations() {
+		return listOfNavigations;
+	}
+}
--- core/src/main/java/com/usbank/loyality/core/utils/CommonUtil.java
+++ core/src/main/java/com/usbank/loyality/core/utils/CommonUtil.java
@@ -1,8 +1,11 @@
 package com.usbank.loyality.core.utils;
 
-import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.usbank.loyality.core.constants.GlobalConstants;
 
 /**
  * @author ajena
@@ -27,4 +30,20 @@ public class CommonUtil {
         }
         return returnValue;
     }
+    
+    /**
+     * Checks if is new window.
+     *
+     * @param newWindow the new window
+     * @return the string
+     */
+    public static String isNewWindow(String newWindow) {
+        log.debug("In isNewWindow method of CommonUtil component.");
+        if (StringUtils.isNotBlank(newWindow) && newWindow.equalsIgnoreCase(GlobalConstants.TRUE)) {
+            return GlobalConstants.TARGET_BLANK;
+        } else {
+            return GlobalConstants.TARGET_SELF;
+        }
+    }
+
 }
